f42bf38b7c7e9b15a2f78ed8600b5089338aaeb7
thizinRodrigues <112214264+thizinrodrigues@users.noreply.github.com>
2023-05-09 23:22:21 -0300

    Add files via upload

--- select.php
+++ select.php
@@ -39,3 +39,18 @@ $varriavel = new conselect();
 $varriavel->ler();
 
 ?>
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>index</title>
+</head>
+<body>
+<p> 
+    <a href="index.php" class="btn btn-danger">Return</a>
+</p>
+
+</body>
+
+</html>
